7b545ed9f52331418293c47a905989fafe649453
Miles Gray <mgray16@gmail.com>
2020-10-20 09:20:11 -0500

    Add error handling in package level init

--- augtistic/__init__.py
+++ augtistic/__init__.py
@@ -1,6 +1,12 @@
-from .utils import check_tf_version
-check_tf_version()
+try:
+    from .utils import check_tf_version
+    check_tf_version()
+except:
+    print(f"[WARNING]\t Augtistic could not check TensorFlow version, it should be at least 2.3!")
 
-#from .layers import *
+try:
+    from .layers import *
+except:
+    print(f"[WARNING]\t Augtistic could not import convenience access to layers, use `import augtistic.layers` instead. Sorry.")
 
 from .version import __version__
